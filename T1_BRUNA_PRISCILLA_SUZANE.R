42de40a24c08a80a19dbc545adea7fcb30982f2c
bruna <brunaol678@gmail.com>
2019-06-01 09:22:05 -0300

    revisão da questão 2

--- T1_BRUNA_PRISCILLA_SUZANE.R
+++ T1_BRUNA_PRISCILLA_SUZANE.R
@@ -23,25 +23,14 @@ nomero(name)
 ###############################################
 
 # QUESTÃO 2
-# AINDA NÃO FOI REVISADA
-
-#Utilize set.seed(m) e gere 10*m observações de uma normal de média m e desvio padrão s. 
-#Faça o histograma e obtenha as principais medidas de posição dos valores simulados
 
 name = 'bruna priscila suzane'
-m = nomero(name)
+m = nomero('bruna priscila suzane')
 s<-m/4
 set.seed(m)
+n <- rnorm(10*m,m,s)
+hist(n,plot=TRUE)
 
-for (i in 1:10) {
-  print(rnorm(m))
-}
-m
-pnorm(m+s, mean = m, sd = s)
-n=10*m
-# Distribuição Normal ou gaussiana
-par(mfrow=c(2,2))
-curve(dnorm(x),-s,s,add = F, col = 'orange')
 
 ###############################################
 
